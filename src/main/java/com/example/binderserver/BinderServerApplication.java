a07adf2a5e91dcc8b97e5088abb1db1ed7e80e33
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-01 21:01:46 +0900

    feat: exclude dataSourceAutoConfiguration.class

--- src/main/java/com/example/binderserver/BinderServerApplication.java
+++ src/main/java/com/example/binderserver/BinderServerApplication.java
@@ -2,8 +2,9 @@ package com.example.binderserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BinderServerApplication {
 
 	public static void main(String[] args) {
