8f74a213fcad1d6212a9de95bf91cda8388453bb
Azu <azuping@gmail.com>
2022-12-01 16:26:09 +0900

    Initial setup

---
+++ src/main/java/com/example/binderserver/BinderServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.binderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinderServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/binderserver/BinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
