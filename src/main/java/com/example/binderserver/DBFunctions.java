7a9d48b46b2a3378fffab9ebe2d5d78b60b4258f
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-03 18:42:41 +0900

    feat: add functions that create user_info, user_books, reputation and trade tables.

--- src/main/java/com/example/binderserver/DBFunctions.java
+++ src/main/java/com/example/binderserver/DBFunctions.java
@@ -20,7 +20,7 @@ public class DBFunctions {
         }
         return conn;
     }
-    public void createTable(Connection conn, String table_name) {
+    public void create_user_info_table(Connection conn, String table_name) {
         Statement statement;
         try{
             String query="create table "+table_name+" (id SERIAL,username varchar(200),city varchar(200),postal_code varchar(200),phone_number varchar(200),reputation float,isBanned boolean,primary key(id));";
@@ -32,10 +32,46 @@ public class DBFunctions {
         }
     }
 
+    public void create_trade_table(Connection conn, String table_name) {
+        Statement statement;
+        try{
+            String query="create table "+table_name+" (id SERIAL,sender int, receiver int, book_id int, is_matched boolean, is_accepted boolean, is_exchanged int, primary key(id));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created!");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void create_user_books_table(Connection conn, String table_name) {
+        Statement statement;
+        try{
+            String query="create table "+table_name+" (id SERIAL,user_id int,isbn varchar(200),condition varchar(200),primary key(id));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created!");
+        }catch (Exception e) {
+            System.out.println(e + "🔥");
+        }
+    }
+
+    public void create_reputation_table(Connection conn, String table_name) {
+        Statement statement;
+        try{
+            String query="create table "+table_name+" (id SERIAL,recipient int, reveiwer int, score int, primary key(id));";
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Table Created!");
+        }catch (Exception e) {
+            System.out.println(e + "🔥");
+        }
+    }
+
     public void insert_row(Connection conn, String table_name,String username, String city, String postal_code, String phone_number, float reputation, boolean isBanned){
         Statement statement;
         try{
-            String query=String.format("insert into %s(username,city,postal_code,phone_number,reputation,isBanned) values ('%s','%s');",table_name,username,city,postal_code,phone_number,reputation,isBanned);
+            String query=String.format("insert into %s(username,city,postal_code,phone_number,reputation,isBanned) values ('%s','%s','%s','%s','%s','%s');",table_name,username,city,postal_code,phone_number,reputation,isBanned);
             statement=conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Row Inserted!");
