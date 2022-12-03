d92559b785cd6beb678ef30cce5b5271f9679150
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-03 18:41:16 +0900

    feat: add migration implementations

--- src/main/java/com/example/binderserver/BinderServerApplication.java
+++ src/main/java/com/example/binderserver/BinderServerApplication.java
@@ -12,7 +12,10 @@ public class BinderServerApplication {
 	public static void main(String[] args) {
 		DBFunctions db=new DBFunctions();
 		Connection conn=db.connect_to_db("binderdb","postgres","helloworld");
-//		db.createTable(conn,"user_info");
+		db.create_user_info_table(conn, "user_info");
+		db.create_user_books_table(conn,"user_books");
+		db.create_reputation_table(conn, "reputation");
+		db.create_trade_table(conn,"trade");
 		db.insert_row(conn,"user_info","gardocaves","Kawasaki","14134","08067861944",1,false);
 	}
 }
