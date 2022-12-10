c0ba7bef5089b73a8b67ee279f281ddb3f40f82a
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-10 12:12:48 +0900

    fix: trade_tables to trade_table

--- src/main/java/com/binder/server/controller/TradeTableController.java
+++ src/main/java/com/binder/server/controller/TradeTableController.java
@@ -20,23 +20,23 @@ public class TradeTableController {
         this.tradeTableRepository = tradeTableRepository;
     }
 
-    @GetMapping("trade_tables")
+    @GetMapping("trade_table")
     public List<TradeTable> getAllTradeTables() {
         return this.tradeTableRepository.findAll();
     }
 
-    @GetMapping("trade_tables/{id}")
+    @GetMapping("trade_table/{id}")
     public ResponseEntity<TradeTable> getUserTableById(@PathVariable(value = "id") Long tradeTableId)
      throws ResourceNotFoundException {
         TradeTable tradeTable = tradeTableRepository.findById(tradeTableId)
                 .orElseThrow(() -> new ResourceNotFoundException("TradeTable not found for this id :: " + tradeTableId));
         return ResponseEntity.ok().body(tradeTable);
  }
-    @PostMapping("user_table")
+    @PostMapping("trade_table")
     public TradeTable createTradeTable(@RequestBody TradeTable tradeTable) {
         return this.tradeTableRepository.save(tradeTable);
  }
-    @PutMapping("trade_tables/{id}")
+    @PutMapping("trade_table/{id}")
     public ResponseEntity<TradeTable> updateTradeTable(@PathVariable(value ="id") Long tradeTableId,
                                                        @Validated @RequestBody TradeTable tradeTableDetails) throws ResourceNotFoundException {
         TradeTable tradeTable = tradeTableRepository.findById(tradeTableId)
@@ -51,7 +51,7 @@ public class TradeTableController {
         return ResponseEntity.ok(this.tradeTableRepository.save(tradeTable));
     }
 
-    @DeleteMapping("trade_tables/{id}")
+    @DeleteMapping("trade_table/{id}")
     public Map<String, Boolean> deleteTradeTable(@PathVariable(value = "id") Long tradeTableId) throws ResourceNotFoundException {
         TradeTable tradeTable = tradeTableRepository.findById(tradeTableId)
                 .orElseThrow(() -> new ResourceNotFoundException("TradeTables not found for this id ::" + tradeTableId));
@@ -63,10 +63,13 @@ public class TradeTableController {
         return response;
     }
 
-    @GetMapping("trade_tables/sender/{id}")
+    @GetMapping("trade_table/sender/{id}")
     public List<TradeTable> findByBooksNotByUserId(@PathVariable(value = "id") int id){
         return this.tradeTableRepository.findBySender(id);
     }
 
-
+    @PostMapping("trade_table/match")
+    public List<TradeTable> findSenderAndReceiver(@Validated @RequestBody TradeTable tradeTableDetails) {
+        return this.tradeTableRepository.findBySenderAndReceiver(tradeTableDetails.getReceiver(), tradeTableDetails.getSender());
+    }
 }
