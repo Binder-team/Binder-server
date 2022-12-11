5d03155d882a778b3004f5c3a586f8aced0a184a
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-11 22:16:48 +0900

    fix: change id data types to Long

--- src/main/java/com/binder/server/repository/UserBooksRepository.java
+++ src/main/java/com/binder/server/repository/UserBooksRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface UserBooksRepository extends JpaRepository<UserBooks, Long> {
 
-    List<UserBooks> findByUserId(int userId);
-    List<UserBooks> findByUserIdNot(int userId);
+    List<UserBooks> findByUserId(Long userId);
+    List<UserBooks> findByUserIdNot(Long userId);
 }
