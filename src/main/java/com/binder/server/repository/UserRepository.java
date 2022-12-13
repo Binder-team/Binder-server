ce89804e3167a8ff950018a132cb35b94212dc1b
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-13 14:17:59 +0900

    feat: add findByUser method

--- src/main/java/com/binder/server/repository/UserRepository.java
+++ src/main/java/com/binder/server/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserById(Long id);
     User findUserByUsername(String username);
 }
 
