8bbcef7d8115ca43669a2a5ecc4f396321662422
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-16 15:52:12 +0900

    feat: create response body for put requests

--- src/main/java/com/binder/server/controller/MatchController.java
+++ src/main/java/com/binder/server/controller/MatchController.java
@@ -7,6 +7,7 @@ import com.binder.server.model.UserBooks;
 import com.binder.server.repository.MatchRepository;
 import com.binder.server.repository.UserBooksRepository;
 import com.binder.server.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -52,19 +53,23 @@ public class MatchController {
     }
 
     @PutMapping("matches/accept/user/{username}")
-    public void acceptTrade(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
+    public ResponseEntity<Match> acceptTrade(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
         UserBooks book1 = userBooksRepository.findUserBooksById(matchDetails.getBook1Id());
         UserBooks book2 = userBooksRepository.findUserBooksById(matchDetails.getBook1Id());
         book1.setIsAvailable(false);
         book2.setIsAvailable(false);
+        this.userBooksRepository.save(book1);
+        this.userBooksRepository.save(book2);
         if (username == matchDetails.getUsername1()){
             matchDetails.setDidUser1Accept(true);
         } else matchDetails.setDidUser2Accept(true);
         this.matchRepository.save(matchDetails);
+
+        return ResponseEntity.ok().body(matchDetails);
     }
 
     @PutMapping("matches/deny/user/{username}")
-    public void denyTrade(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
+    public ResponseEntity<Match> denyTrade(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
         UserBooks book1 = userBooksRepository.findUserBooksById(matchDetails.getBook1Id());
         UserBooks book2 = userBooksRepository.findUserBooksById(matchDetails.getBook1Id());
         Match matchRecord = matchRepository.findMatchById(matchDetails.getId());
@@ -72,20 +77,18 @@ public class MatchController {
         book2.setIsAvailable(true);
         this.userBooksRepository.save(book1);
         this.userBooksRepository.save(book2);
-        if (username == matchDetails.getUsername1()){
-            matchDetails.setDidUser1Accept(false);
-        } else matchDetails.setDidUser2Accept(false);
-        this.matchRepository.save(matchDetails);
-        if (matchDetails.getDidUser1Accept() == false && matchDetails.getDidUser2Accept() == false) {
-            this.matchRepository.delete(matchRecord);
-        }
+        this.matchRepository.delete(matchRecord);
+
+        return ResponseEntity.ok().body(matchRecord);
     }
     
     @PutMapping("matches/exchange/user/{username}")
-    public void booksExchanged(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
+    public ResponseEntity<Match> booksExchanged(@PathVariable(value = "username") String username, @RequestBody Match matchDetails) {
         if (username == matchDetails.getUsername1()){
             matchDetails.setDidUser1Exchange(true);
         } else matchDetails.setDidUser2Exchange(true);
         this.matchRepository.save(matchDetails);
+
+        return ResponseEntity.ok().body(matchDetails);
     }
 }
